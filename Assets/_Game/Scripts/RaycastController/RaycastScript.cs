09faf6118eb458457e2f4317116afcab3b5e0a27
İzzet Kadir Eryılmaz <izzet.eryilmaz01@gmail.com>
2025-07-06 02:59:17 +0300

    Dream System

---
+++ Assets/_Game/Scripts/RaycastController/RaycastScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class RaycastScript : MonoBehaviour
+{
+    public static RaycastScript instance;
+    public float rayDistance = 5f; // Ray'in ne kadar uzaða gideceði
+    public Transform[] spawnpoints;
+    CharacterController controller;
+    public Canvas TimerCanvas;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                Debug.Log("Çarptýðýn nesne: " + hit.collider.name);
+
+                switch (hit.collider.tag)
+                {
+                    case "Yatak":
+                        if (spawnpoints.Length > 0)
+                        {
+                            StartCoroutine(TeleportTo(spawnpoints[0].position));
+                        }
+                        else
+                        {
+                            Debug.Log("Rüyaya Dalýyorsunuz.");
+                        }
+                    break;
+
+                    default:
+                        Debug.Log("Hiçbir iþlem yapýlmadý.");
+                        break;
+                }
+            }
+            else
+            {
+                Debug.Log("Hiçbir þeye çarpmadý.");
+            }
+        }
+
+    }
+
+    private IEnumerator TeleportTo(Vector3 targetPos)
+    {
+        controller.enabled = false;      // CharacterController'ý devre dýþý býrak
+        transform.position = targetPos;  // Pozisyonu deðiþtir
+        yield return null;               // Bir frame bekle
+        controller.enabled = true;       // Tekrar aktif et
+    }
+}
