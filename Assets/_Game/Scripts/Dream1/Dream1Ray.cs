5dfa8686d3acdc9c64248ec4e9574bd65cdc18ba
İzzet Kadir Eryılmaz <izzet.eryilmaz01@gmail.com>
2025-07-22 12:46:48 +0300

    KarakterDüzenleme

--- Assets/_Game/Scripts/Dream1/Dream1Ray.cs
+++ Assets/_Game/Scripts/Dream1/Dream1Ray.cs
@@ -6,7 +6,7 @@ public class Dream1Ray : MonoBehaviour
 {
     private int rayDistance = 5;
     public LayerMask interactableLayer;
-    bool Emzik, Ayýcýk, Çýngýrak;
+    bool Pacifier, Bear, Hammer;
     public TMP_Text CollectedText;
     public AudioSource PickUpSound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,38 +28,35 @@ public class Dream1Ray : MonoBehaviour
             {
                 switch (hit.collider.tag)
                 {
-                    case "Emzik":
-                        Emzik = true;
-                        Debug.Log("Emzik Toplandý");
+                    case "Pacifier":
+                        Pacifier = true;
                         PickUpSound.Play();
-                        Animator EmzikAnimator = hit.collider.gameObject.GetComponent<Animator>();
+                        Animator PacifierAnimator = hit.collider.gameObject.GetComponent<Animator>();
                         PlayerPrefs.SetInt("Collected", PlayerPrefs.GetInt("Collected") + 1);
-                        EmzikAnimator.Play("EmzikAnim");
+                        PacifierAnimator.Play("PacifierAnim");
                         BölümKontrol();
                         break;
 
-                    case "Ayýcýk":
-                        Ayýcýk = true;
-                        Debug.Log("Ayýcýk Toplandý");
+                    case "Bear":
+                        Bear = true;
                         PickUpSound.Play();
-                        Animator AyýcýkAnimator = hit.collider.gameObject.GetComponent<Animator>();
+                        Animator BearAnimator = hit.collider.gameObject.GetComponent<Animator>();
                         PlayerPrefs.SetInt("Collected", PlayerPrefs.GetInt("Collected") + 1);
-                        AyýcýkAnimator.Play("AyýcýkAnim");
+                        BearAnimator.Play("BearAnim");
                         BölümKontrol();
                         break;
 
-                    case "Çýngýrak":
-                        Çýngýrak = true;
-                        Debug.Log("Çýngýrak Toplandý");
+                    case "Hammer":
+                        Hammer = true;
                         PickUpSound.Play();
-                        Animator ÇýngýrakAnimator = hit.collider.gameObject.GetComponent<Animator>();
+                        Animator HammerAnimator = hit.collider.gameObject.GetComponent<Animator>();
                         PlayerPrefs.SetInt("Collected", PlayerPrefs.GetInt("Collected") + 1);
-                        ÇýngýrakAnimator.Play("ÇýngýrakAnim");
+                        HammerAnimator.Play("HammerAnim");
                         BölümKontrol();
                         break;
 
                     default:
-                        Debug.Log("Hiçbir iþlem yapýlmadý.");
+                        Debug.Log("Hicbir islem yapilmadi.");
                         break;
                 }
             }
@@ -70,11 +67,11 @@ public class Dream1Ray : MonoBehaviour
     private void BölümKontrol()
     {
         CollectedText.text = PlayerPrefs.GetInt("Collected").ToString()+"/3";
-        if (Emzik == true && Ayýcýk == true && Çýngýrak == true)
+        if (Pacifier == true && Bear == true && Hammer == true)
         {
             PlayerPrefs.SetInt("Dream1WakeUp", 1);
             SceneManager.LoadScene("HospitalRoomMap");
-            Debug.Log("Bölüm Kazanýldý");
+            Debug.Log("Bölüm Kazanildi.");
         }
         else { return; }
     }
